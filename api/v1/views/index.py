3f67ad0cb592b4519fa81bfc981ec5d5a55e9b5d
chasenaidoo <chasenaidoo9@gmail.com>
2024-01-06 04:36:31 -0800

    Modified index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,12 +15,12 @@ def status():
 def get_stats():
     """Retrieve the number of each object by type"""
     model_counts = {
-        amenities=storage.count("Amenity"),
-        cities=storage.count("City"),
-        places=storage.count("Place"),
-        reviews=storage.count("Review"),
-        states=storage.count("State"),
-        users=storage.count("User")
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
     }
 
     return jsonify(model_counts)

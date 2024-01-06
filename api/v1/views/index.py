fe49923c783e6b73ed3c343ac4474ec60eb5cf6a
chasenaidoo <chasenaidoo9@gmail.com>
2024-01-06 02:53:00 -0800

    Added app.py and index.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a Flask route"""
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """Returns a JSON response with the status OK"""
+    return jsonify({"status": "OK"})

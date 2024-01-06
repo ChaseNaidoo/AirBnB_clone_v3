bd575f17aca0a31f9906939d6ef76857c5797234
chasenaidoo <chasenaidoo9@gmail.com>
2024-01-06 04:46:29 -0800

    Modified index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,11 +14,13 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     """Retrieve the number of each object by type"""
-    return jsonify({
+    stats = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
         "users": storage.count("User")
-    })
+    }
+
+    return jsonify(stats)

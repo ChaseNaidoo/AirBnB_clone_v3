a9f9b6fffe003638cd4beda09d164bdc132ba526
chasenaidoo <chasenaidoo9@gmail.com>
2024-01-06 09:34:34 -0800

    Added handler 404 in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,12 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))

5f13e7e2837e6a63a7f52d114647ea8aa1735f1f
chasenaidoo <chasenaidoo9@gmail.com>
2024-01-06 03:54:58 -0800

    Modified index.py to list classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -14,8 +20,13 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     """Retrieve the number of each object by type"""
-    model_counts = {}
-    for model_name in storage.classes:
-        model_counts[model_name] = storage.count(model_name)
+    model_counts = {
+        "Amenity": storage.count(Amenity),
+        "City": storage.count(City),
+        "Place": storage.count(Place),
+        "Review": storage.count(Review),
+        "State": storage.count(State),
+        "User": storage.count(User)
+    }
 
     return jsonify(model_counts)

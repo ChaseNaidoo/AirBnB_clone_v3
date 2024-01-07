77a17b243c42ea144b91d6b5ee030ee7cfe5964e
chasenaidoo <chasenaidoo9@gmail.com>
2024-01-07 09:38:40 -0800

    Modified base_model.py and user.py to avoid storing password in plain text

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """
 Contains class BaseModel
 """
-
 from datetime import datetime
 import models
 from os import getenv
@@ -58,7 +57,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, include_password=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,6 +67,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if not include_password and "password" in new_dict:
+            del new_dict["password"]
         return new_dict
 
     def delete(self):
